e3234adff92eba0b608fe238d8e01bb08264fde7
willy031110 <411630063@o365.tku.edu.tw>
2024-10-26 10:22:22 +0800

    update

--- tspractice1.js
+++ tspractice1.js
@@ -1,6 +1,4 @@
 var age = 25;
 var Name = "Alice";
-console.log("Initial age: ".concat(age));
-console.log("Initial Name: ".concat(Name));
 Name = "Bob";
-console.log("Updated Name: ".concat(Name));
+age = 30;
--- tspractice1.ts
+++ tspractice1.ts
@@ -1,6 +1,5 @@
+
 const age: number = 25;
 let Name: string = "Alice";
-console.log(`Initial age: ${age}`);
-console.log(`Initial Name: ${Name}`);
-Name = "Bob";
-console.log(`Updated Name: ${Name}`);
+Name = "Bob"; 
+age=30;
